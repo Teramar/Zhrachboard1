8c68ecb3328d1d762629ea1431c6842d7734ec3f
Igor <manstarden@gmail.com>
2020-01-11 21:18:35 +0400

    pupubzduh

--- venvy/Scripts/pip3-script.py
+++ venvy/Scripts/pip3-script.py
@@ -1,4 +1,4 @@
-#!C:\Users\Igor\PycharmProjects\zhrachboard\venvy\Scripts\python.exe
+#!C:\Users\Igor\PycharmProjects\zhrachkaboard\venvy\Scripts\python.exe
 # EASY-INSTALL-ENTRY-SCRIPT: 'pip==19.0.3','console_scripts','pip3'
 __requires__ = 'pip==19.0.3'
 import re
