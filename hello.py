1ece2e08de4e40102ad442e4ae873b87ace6eb3b
Igor <manstarden@gmail.com>
2020-01-11 21:54:49 +0400

    kavo

--- hello.py
+++ hello.py
@@ -1,9 +1,13 @@
-from flask import Flask, Response
+from flask import Flask, Response, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return Response('тут хуйня<br/>какаето')
-
+@app.route('/data')
+def data():
+    name = 'Igor'
+    amount = '24'
+    return jsonify(name, amount)
 if __name__ == '__main__':
     app.run()
