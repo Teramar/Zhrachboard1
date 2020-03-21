eb8a7700be079578d8d576cd857e8ad56438f8b9
Igor <manstarden@gmail.com>
2020-03-21 19:37:57 +0400

    Добавление viewport для сжатие входного массива при запросе GET (не до конца)

--- views.py
+++ views.py
@@ -7,7 +7,7 @@ def hello_world() :
     return flask.Response('тут хуйня<br/>какаето')
 
 
-def data() :
+def data():
     diat = {'name' : 'Igor ', 'amount' : '24'}
     resp = flask.Response(response=json.dumps(diat), status=200, mimetype="application/json")
     return resp
@@ -23,3 +23,35 @@ class Check_R(MethodView) :
         return {
             'method' : 'post'
         }
+class comp_arr(MethodView):
+    def get(self)  :
+        lst = [1, 2, 3, 4, 7, 10, 14, 22, 23, 24]
+        result = []
+        tmp = []
+        for i, v in enumerate(lst) :
+            if tmp :
+                if v == tmp[-1] + 1 :
+                    tmp.append(v)
+
+                else :
+                    if len(tmp) == 1 :
+                        result.append(tmp[0])
+
+                    else :
+                        result.append(f'{tmp[0]}-{tmp[-1]}')
+
+                    tmp = [v]
+            else :
+                tmp.append(v)
+
+            if i == len(lst) - 1 :
+                if len(tmp) == 1 :
+                    result.append(tmp[0])
+
+                else :
+                    result.append(f'{tmp[0]}-{tmp[-1]}')
+                    print(', '.join(map(str, result)))
+        return {
+            "request" : [1, 2, 3, 4, 7, 10, 14, 22, 23, 24],
+            "compressed" : &&&&
+            }
