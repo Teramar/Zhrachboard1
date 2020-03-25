8cd81eb780e4afd99b073e29d6b80330109847da
Teramar <manstarden@gmail.com>
2020-03-25 22:47:48 +0400

    Revert "Array compress into GET-request"

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from views import flask, Check_R, data, hello_world, comp_arr
+from views import flask, Check_R, data, hello_world
 
 app = flask.Flask(__name__)
-app.add_url_rule('/checkapproute', endpoint=None, view_func=Check_R.as_view('checkr'), methods=['GET', 'POST'])
-app.add_url_rule('/data', 'data',  view_func=data,  methods=['GET', 'POST'])
+app.add_url_rule('/checkapproute', 'checkr', Check_R, methods=['GET', 'POST'])
+app.add_url_rule('/data', 'data', data, methods=['GET', 'POST'])
 app.add_url_rule('/', 'hello_world', hello_world, methods=['GET', 'POST'])
-app.add_url_rule('/compress', endpoint=None, view_func=comp_arr.as_view('comparr'), methods=['GET'])
 if __name__ == '__main__' :
     app.run()
